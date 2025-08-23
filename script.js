793df7ef636d78540dc69ed635f5c243e030ab76
Hossain Ahmed <hossainpgmr@gmail.com>
2025-08-24 00:34:13 +0600

    login page creating done

---
+++ script.js
@@ -0,0 +1,22 @@
+// login page validation
+
+const loginBtn = document.getElementById("loginBtn");
+const inputNumber = document.getElementById("numberInput");
+const inputPin = document.getElementById("pinNumberInput");
+
+loginBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const inputNumberValue = inputNumber.value;
+  const inputPinValue = inputPin.value;
+  if (
+    inputNumberValue.length === 11 &&
+    inputNumberValue.startsWith("01") &&
+    inputPinValue.length >= 4
+  ) {
+    window.location.href = "./home.html";
+  } else {
+    alert("Your number is not valid or password incorrect");
+  }
+});
+// home page interactive
+
