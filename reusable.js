e56064f48dbd278e54cc4c83c0671a68a6089ccd
Hossain Ahmed <hossainpgmr@gmail.com>
2025-08-25 12:15:14 +0600

    just try to add interactivity

--- reusable.js
+++ reusable.js
@@ -7,23 +7,30 @@ const payBill = document.getElementById("payBill");
 const transactions = document.getElementById("transactions");
 
 logOutBtn.addEventListener("click", function (e) {
+  e.preventDefault();
   window.location.href = "./index.html";
 });
 addMoney.addEventListener("click", function (e) {
+  e.preventDefault();
   window.location.href = "./addMoney.html";
 });
 cashOut.addEventListener("click", function (e) {
+  e.preventDefault();
   window.location.href = "./cashOut.html";
 });
 transferMoney.addEventListener("click", function (e) {
+  e.preventDefault();
   window.location.href = "./transferMoney.html";
 });
 getBonus.addEventListener("click", function (e) {
+  e.preventDefault();
   window.location.href = "./bonus.html";
 });
 payBill.addEventListener("click", function (e) {
+  e.preventDefault();
   window.location.href = "./payBill.html";
 });
 transactions.addEventListener("click", function (e) {
+  e.preventDefault();
   window.location.href = "./transaction.html";
 });
