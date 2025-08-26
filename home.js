60b74a5237d5aa60b6f90904ecc135ae746ab62e
Hossain Ahmed <hossainpgmr@gmail.com>
2025-08-26 12:43:44 +0600

    update: bonus and pay bill service add event handlar

--- home.js
+++ home.js
@@ -43,7 +43,9 @@ document
       document.getElementById("cash-out-amount").value
     );
     const agentNumber = document.getElementById("cash-out-agent-number").value;
-    const agentPinNumber = document.getElementById("cash-out-user-pin-number").value;
+    const agentPinNumber = document.getElementById(
+      "cash-out-user-pin-number"
+    ).value;
     const totalBalance = balance - amountMoney;
     if (agentNumber.length < 11) {
       alert("Your Bank number is not validate");
@@ -51,6 +53,87 @@ document
     }
     if (agentPinNumber.length < 4) {
       alert("Your pin number is not validate");
+      return;
+    }
+
+    if (totalBalance <= 0) {
+      alert("Your balance is not capable ");
+      return;
+    }
+
+    balanceElm.innerText = totalBalance;
+  });
+// transfer money feature
+document
+  .getElementById("transfer-money-feature-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    const balanceElm = document.getElementById("balance");
+    const balance = parseInt(balanceElm.innerText);
+    const amountMoney = parseInt(
+      document.getElementById("transfer-money-amount").value
+    );
+    const userNumber = document.getElementById(
+      "transfer-user-account-number"
+    ).value;
+    const userPinNumber = document.getElementById("transfer-pin-number").value;
+    const totalBalance = balance - amountMoney;
+    if (userNumber.length < 11) {
+      alert("Your Bank number is not validate");
+      return;
+    }
+    if (userPinNumber.length < 4) {
+      alert("Your pin number is not validate");
+      return;
+    }
+    if (totalBalance <= 0) {
+      alert("Your balance is not capable");
+      return;
+    }
+
+    balanceElm.innerText = totalBalance;
+  });
+// get bonus feature
+
+document
+  .getElementById("get-bonus-feature-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    const coupon = "get20";
+    const getMoney = 2000;
+    const couponNumber = document.getElementById("get-bonus-number").value;
+    const balanceElm = document.getElementById("balance");
+    const balance = parseInt(balanceElm.innerText);
+    const totalBalance = balance + getMoney;
+    if (couponNumber !== coupon) {
+      alert("Please, give the valid coupon code.");
+      return;
+    }
+    balanceElm.innerText = totalBalance;
+  });
+// pay bill feature
+document
+  .getElementById("pay-bill-feature-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    const balanceElm = document.getElementById("balance");
+    const balance = parseInt(balanceElm.innerText);
+    const amountMoney = parseInt(
+      document.getElementById("pay-bill-amount").value
+    );
+    const totalBalance = balance - amountMoney;
+    const bankNumber = document.getElementById("pay-bill-bank-number").value;
+    const pinNumber = document.getElementById("pay-bill-pin-number").value;
+    if (bankNumber.length < 11) {
+      alert("Your Bank number is not validate");
+      return;
+    }
+    if (pinNumber.length < 4) {
+      alert("Your pin number is not validate");
+      return;
+    }
+    if (totalBalance <= 0) {
+      alert("Your balance is not capable");
       return;
     }
 
