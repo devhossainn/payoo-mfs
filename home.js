9785f7c24720ee74d53cbdaa8b9d988235b86121
Hossain Ahmed <hossainpgmr@gmail.com>
2025-08-26 11:06:33 +0600

    add money and cashout service interactivity done

--- home.js
+++ home.js
@@ -6,8 +6,56 @@ const getBonusElm = document.getElementById("get-bonus-feature");
 const payBillElm = document.getElementById("pay-bill-feature");
 const transactionElm = document.getElementById("transaction-feature");
 
-// feature functionality
+// AddMoney feature functionality
+document
+  .getElementById("add-money-feature-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    const balanceElm = document.getElementById("balance");
+    const balance = parseInt(balanceElm.innerText);
+    const amountMoney = parseInt(
+      document.getElementById("add-money-amount").value
+    );
+    const totalBalance = balance + amountMoney;
+    const bankNumber = document.getElementById("bank-number").value;
+    const pinNumber = document.getElementById(
+      "add-money-user-pin-number"
+    ).value;
+    if (bankNumber.length < 11) {
+      alert("Your Bank number is not validate");
+      return;
+    }
+    if (pinNumber.length < 4) {
+      alert("Your pin number is not validate");
+      return;
+    }
 
+    balanceElm.innerText = totalBalance;
+  });
+// cash out feature functionality
+document
+  .getElementById("cash-out-feature-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    const balanceElm = document.getElementById("balance");
+    const balance = parseInt(balanceElm.innerText);
+    const amountMoney = parseInt(
+      document.getElementById("cash-out-amount").value
+    );
+    const agentNumber = document.getElementById("cash-out-agent-number").value;
+    const agentPinNumber = document.getElementById("cash-out-user-pin-number").value;
+    const totalBalance = balance - amountMoney;
+    if (agentNumber.length < 11) {
+      alert("Your Bank number is not validate");
+      return;
+    }
+    if (agentPinNumber.length < 4) {
+      alert("Your pin number is not validate");
+      return;
+    }
+
+    balanceElm.innerText = totalBalance;
+  });
 // toggling service feature
 function hideAllFeature() {
   addMoneyElm.classList.replace("block", "hidden");
